91d12b97d54b13048810150f2cc64fe8b0dc39e2
thomsd20 <pdantech@gmail.com>
2017-06-12 22:19:23 +1200

    Finish product loading and create README file

---
+++ README.md
@@ -0,0 +1,19 @@
+# Wall Street Wines
+## ISCG6420 IWD Assignment 2 Website Project
+### By Daniel, Eric and Wayne
+
+Developed using HTML, Javascript and CSS.
+
+Uses various libraries for JS and CSS:
+- JQuery
+- Bootstrap CSS
+- NodeJS
+    - Express
+    - File System Functions
+- Font Awesome
+
+## Running App
+1. Open command line and navigate to app directory
+2. Run `npm start` this will install node packages and start the server
+3. Go to [localhost:3000](http://localhost:3000)
+
--- scripts/products.js
+++ scripts/products.js
@@ -14,47 +14,23 @@ function loadProducts(){
     });
 }
 
-function displayProducts(data){
-    //var data = JSON.parse(json);
 
+/* Using the JSON data returned from our node API we build each product tile and add it to the html page using jQuery */
+function displayProducts(data){
     var $productsDiv = $('#productsDiv');
     for(var i = 0; i < data.length; i++){
         var $link = $("<a href='#'>");
         var $div = $("<div>", {"class": "product"});
         $("<img>").attr("src", data[i].imgSrc).attr("title", data[i].title).appendTo($div);
         var $display = $("<div>", {"class": "dealtext"});
-        $("<h2>").text(data[i].price).appendTo($display);
+        var $price = $("<h2>").text("$" + data[i].price).appendTo($display);
+        $("<span>").attr("class", "fa fa-plus-circle pull-right addToCart").attr("aria-hidden", "true").appendTo($price);
         $("<h4>").text(data[i].title).appendTo($display);
         $div.append($display);
         $productsDiv.append($div);
     }
 }
 
-/*<a href="#">
-        <div class="product">
-          <img src="images/oyster bay wine.jpg" title="Image from https://www.queenstownnz.co.nz/explore/listing/luma-southern-light-project">
-          <div class="dealtext">
-            <h2>$10.99</h2>
-            <h4>Oyster Bay Wine</h4>
-
-          </div>
-        </div>
-      </a>*/
-
-/*
-[
-    {
-        "title": "Wine 1",
-        "description": "This is a nice wine!",
-        "imgSrc": "images/wine1.jpg",
-        "type": "red",
-        "price": 5.0
-    },
-    {
-        "title": "Wine 2",
-        "description": "This is also a nice wine!",
-        "imgSrc": "images/wine2.jpg",
-        "type": "white",
-        "price": 5.0
-    }
-]*/
+function addToCart(product) {
+    
+}
